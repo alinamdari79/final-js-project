94cf1e53394a31444894fc26932d72ae2e5e2eed
alinamdari79 <90065028+alinamdari79@users.noreply.github.com>
2024-11-16 23:55:38 +0330

    first develop of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,}", "./main.js"],
+  content: ["./index.html", "./src/**/*.js", "./main.js"],
   theme: {
     extend: {},
   },
